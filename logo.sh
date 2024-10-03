029f633d5362bec1bf985d537e813593e383ad11
Elessar <108255403+enyaselessar@users.noreply.github.com>
2024-10-03 12:49:32 +0600

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -1,7 +1,8 @@
-echo -e "\033[0;34m"  # Change this to blue
+# Use the color variable from the calling script
+echo -e "${BLUE}"
 echo "       _                           "
 echo "   ___| | ___  ___ ___  __ _ _ __  "
 echo "  / _ \ |/ _ \/ __/ __|/ _  |  __| "
 echo " |  __/ |  __/\__ \__ \ (_| | |    "
 echo "  \___|_|\___||___/___/\__,_|_|    "
-echo -e "\e[0m"  # Reset color
+echo -e "${NC}"  # Reset color to no color
