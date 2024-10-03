af115d2db0881370a8d4a2a469abf904a53e6e79
Elessar <108255403+enyaselessar@users.noreply.github.com>
2024-10-03 12:47:00 +0600

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -1,7 +1,7 @@
-echo -e "\033[0;34m"
+echo -e "\033[0;34m"  # Change this to blue
 echo "       _                           "
 echo "   ___| | ___  ___ ___  __ _ _ __  "
 echo "  / _ \ |/ _ \/ __/ __|/ _  |  __| "
 echo " |  __/ |  __/\__ \__ \ (_| | |    "
 echo "  \___|_|\___||___/___/\__,_|_|    "
-echo -e "\e[0m"
+echo -e "\e[0m"  # Reset color
